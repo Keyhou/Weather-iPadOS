dd6012dde95097c4dc5793cb7d0affc6bb4c74cf
Keyhou <31210145+Keyhou@users.noreply.github.com>
2021-12-06 18:11:46 +0100

    Initial Commit

---
+++ Weather App/Weather_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Weather_AppApp.swift
+//  Weather App
+//
+//  Created by Keyhan Mortezaeifar on 06/12/21.
+//
+
+import SwiftUI
+
+@main
+struct Weather_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
